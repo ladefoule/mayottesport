bb1f42534ac72a45daca3ddb791673c2bbd722fa
Moussa <moussa@976>
2020-11-10 22:51:11 +0100

    Version navbar white + gray

--- resources/views/layouts/competition.blade.php
+++ resources/views/layouts/competition.blade.php
@@ -28,18 +28,18 @@ $sports = Sport::all();
     {{-- <div class="d-flex justify-content-center bg-white p-2">
         <a href="/" class="col-10 col-sm-12 d-flex justify-content-center"></a>
     </div> --}}
-    <nav class="navbar sticky-top navbar-light navbar-expand-lg border-bottom" style="background-color: #fff !important">
-        <div class="container">
-            <a class="navbar-brand" href="#"></a>
+    <nav class="navbar sticky-top navbar-light navbar-expand-lg border-bottom bg-white border p-0">
+        <div class="container px-3">
+            <a class="navbar-brand" href="/"><img class="img-fluid mx-auto" src="/storage/img/logo-mayottesport-com.jpg" alt="Logo MayotteSport" style="height: 40px"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <div class="navbar-nav mr-auto bg-white d-flex align-items-center text-left">
-                    <a class="nav-item nav-link px-3" href="/"><img class="img-fluid mx-auto" src="/storage/img/logo-mayottesport-com.jpg" alt="Logo MayotteSport" style="height: 30px"></a>
+                    {{-- <a class="nav-item nav-link px-3" href="/"><img class="img-fluid mx-auto" src="/storage/img/logo-mayottesport-com.jpg" alt="Logo MayotteSport" style="height: 30px"></a> --}}
                     @foreach ($sports as $sport)
-                        <a class="nav-item nav-link @if (strToUrl($sport->nom) == $sportNom) active text-dark @endif px-3" href="/{{ strToUrl($sport->nom) }}">{{ $sport->nom }}</a>
+                        <a class="nav-item nav-link @if (strToUrl($sport->nom) == $sportNom) active text-body @endif px-3" href="/{{ strToUrl($sport->nom) }}">{{ $sport->nom }}</a>
                     @endforeach
                     {{-- <a class="nav-item nav-link px-3" href="/handball">Handball</a>
                     <a class="nav-item nav-link px-3" href="/basketball">Basketball</a>
@@ -54,23 +54,27 @@ $sports = Sport::all();
     <div class="container-lg-fluid" style="background-color: rgba(151, 153, 153, 0.7) !important">
         <div class="container-lg">
             <div class="row overflow-x-auto py-3 mx-0" id="navbar-scroll-x">
-                <div class="d-flex justify-content-center align-items-center pl-3" style="margin:0;font-size:17px">
-                    <a href="" class="text-dark font-weight-bold pr-3">
+                <div class="d-flex justify-content-start align-items-center pl-3" style="margin:0;font-size:0.9rem">
+                    <a class="flex-shrink-0 text-dark font-weight-bold pr-3 float-left">
                         {{ Str::upper($competition) }}
                     </a>
-                    <a href="" class="text-white font-weight-bold pr-3">
+                    <span class="pr-3">
+                        <i class="fas fa-angle-right"></i>
+                    </span>
+                    <a href="" class="flex-shrink-0 text-white font-weight-bold pr-3">
                         Le classement
                     </a>
-                    <a href="" class="text-dark font-weight-bold pr-3">
+                    <a href="" class="d-flex flex-shrink-0 text-dark font-weight-bold pr-3">
                         Les derniers résultats
                     </a>
-                    <a href="" class="text-dark font-weight-bold">
+                    <a href="" class="flex-shrink-0 text-dark font-weight-bold">
                         La prochaine journée
                     </a>
-                    {{-- <a href="">
-                        <button class="btn btn-sm mx-2 px-3 btn-light">Classement</button>
-                    </a> --}}
                 </div>
+
+                {{-- <a href="">
+                    <button class="btn btn-sm mx-2 px-3 btn-light">Classement</button>
+                </a> --}}
             </div>
         </div>
     </div>
