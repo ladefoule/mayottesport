2d2808a5903423f42f5a6bf053a725f85036ef3d
Moussa <moussa@976>
2020-11-19 10:15:15 +0100

    Commit avant création de la branche VueJS

--- resources/views/competition/day.blade.php
+++ resources/views/competition/day.blade.php
@@ -5,19 +5,19 @@
 @section('content')
 
 <div class="row d-flex flex-wrap m-0 bg-white rounded p-3">
-    <div class="col-12 d-flex flex-wrap justify-content-center align-items-center pb-3">
-        <h1 class="h4 text-center p-2 col-12">{{ request()->competition->nom . ' - Calendrier et résultats'}}</h1>
+    <h1 class="h4 text-center p-2 col-12">{{ request()->competition->nom . ' - Calendrier et résultats'}}</h1>
+    <div class="col-12 d-flex flex-nowrap justify-content-center align-items-center pb-3">
         @if ($hrefJourneePrecedente)
-            <a href="{{ $hrefJourneePrecedente }}" class="float-right pr-3">précédente</a>
+            <a href="{{ $hrefJourneePrecedente }}" class="float-right pr-2">précédente</a>
         @endif
-        <select class="form-control col-6 col-sm-4 col-md-3" name="journee" id="journee">
+        <select class="form-control col-6 col-sm-4 col-md-3 px-2" name="journee" id="journee">
             @foreach ($journees as $journee_)
                 <option data-href="{{ route('competition.day', ['sport' => strToUrl(request()->sport->nom),'competition' => strToUrl(request()->competition->nom),'journee' => $journee_->numero]) }}"
                     value="{{ $journee_->numero }}" @if($journee->numero == $journee_->numero) selected @endif>{{ niemeJournee($journee_->numero) }}</option>
             @endforeach
         </select>
         @if ($hrefJourneeSuivante)
-            <a href="{{ $hrefJourneeSuivante }}" class="float-left pl-3">suivante</a>
+            <a href="{{ $hrefJourneeSuivante }}" class="float-left pl-2">suivante</a>
         @endif
     </div>
     <div class="col-lg-8 d-flex flex-wrap p-0">
--- resources/views/errors/illustrated-layout.blade.php
+++ resources/views/errors/illustrated-layout.blade.php
@@ -471,8 +471,9 @@
                     </p>
 
                     <a href="{{ app('router')->has('home') ? route('home') : url('/') }}">
-                        <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 px-6 border-2 border-grey-light hover:border-grey rounded-lg">
-                            ACCUEIL
+                        <button class="bg-transparent text-grey-darkest font-bold uppercase tracking-wide py-3 {{-- px-6 --}} border-2 border-grey-light hover:border-grey rounded-lg">
+                            {{-- ACCUEIL --}}
+                            <img class="img-fluid" src="/storage/img/logo-mayottesport-com.jpg" alt="Accueil" style="width:75%">
                         </button>
                     </a>
                 </div>
