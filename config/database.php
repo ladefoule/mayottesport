953bbdc85b3336b73105a8f4f3ac5a32124e3bda
Moussa ALI MOUSSA <moussa.ali.pro@gmail.com>
2020-12-22 22:22:39 +0000

    MAJ Config de redis

--- config/database.php
+++ config/database.php
@@ -119,7 +119,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),//phpredis
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -141,7 +141,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'database' => env('REDIS_CACHE_DB', 1),
         ],
 
     ],
