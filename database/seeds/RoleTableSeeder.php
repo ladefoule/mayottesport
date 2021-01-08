713a65ea4de357c19254f53e7815c24550fe55ef
ALI MOUSSA <ali@ubuntu-new>
2021-01-09 00:19:16 +0100

    Niveau dans les seeds

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -13,10 +13,10 @@ class RoleTableSeeder extends Seeder
     {
         // On insère les différentes catégories d'user
         $roles = [
-            ['name' => 'membre', 'guard_name' => 'web'],
-            ['name' => 'premium', 'guard_name' => 'web'],
-            ['name' => 'admin', 'guard_name' => 'web'],
-            ['name' => 'superadmin', 'guard_name' => 'web']
+            ['name' => 'membre', 'guard_name' => 'web', 'niveau' => 10],
+            ['name' => 'premium', 'guard_name' => 'web', 'niveau' => 20],
+            ['name' => 'admin', 'guard_name' => 'web', 'niveau' => 30],
+            ['name' => 'superadmin', 'guard_name' => 'web', 'niveau' => 40]
         ];
         foreach ($roles as $role) {
             App\Role::create($role);
