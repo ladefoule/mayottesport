e32e5c9d1dd6e787d4c8903993b9fc772fed03c8
ALI MOUSSA <ali@ubuntu-new>
2021-01-29 00:45:01 +0100

    test Pub

--- resources/views/onglet/section-content-home-et-sport.blade.php
+++ resources/views/onglet/section-content-home-et-sport.blade.php
@@ -25,6 +25,20 @@
     </div>
 
     <div class="col-12 d-lg-none bg-white">
+        <div class="col-12">
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- v2 -->
+            <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-6802785230681286"
+                data-ad-slot="8301161659"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
+        
         <div id="actualites-content" class="pb-3 @if(! $articles) d-none @endif">
             <div class="d-flex flex-wrap justify-content-start align-items-stretch">
                 {!! $articles !!}
