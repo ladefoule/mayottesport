c1e0ace26d8514381d8fe2977da70d747c90eab9
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-01-31 19:38:12 +0100

    STS header

--- resources/views/layouts/include/header.blade.php
+++ resources/views/layouts/include/header.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php header('Strict-Transport-Security: max-age=63072000'); ?> {{-- Only connect to this site via HTTPS for the two years (recommended) --}}
+    <?php header('Strict-Transport-Security: max-age=63072000; includeSubDomains; preload'); ?> {{-- Only connect to this site via HTTPS for the two years (recommended) --}}
     @yield('head')
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/storage/img/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
