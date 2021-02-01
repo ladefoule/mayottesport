a885b1092a92a68778938c0bd4af7f208c7c00b3
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-01 11:20:54 +0100

    CSP Deny

--- resources/views/layouts/include/header.blade.php
+++ resources/views/layouts/include/header.blade.php
@@ -4,7 +4,15 @@
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <?php header('Strict-Transport-Security: max-age=63072000; includeSubDomains; preload'); ?> {{-- Only connect to this site via HTTPS for the two years (recommended) --}}
+    <?php
+        // Only connect to this site via HTTPS for the two years (recommended)
+        header('Strict-Transport-Security: max-age=63072000; includeSubDomains; preload'); 
+
+        # Block site from being framed with X-Frame-Options and CSP
+        header("Content-Security-Policy: frame-ancestors 'none'");
+        header("X-Frame-Options: DENY");
+    ?> 
+
     @yield('head')
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/storage/img/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset(mix('css/app.css')) }}">
