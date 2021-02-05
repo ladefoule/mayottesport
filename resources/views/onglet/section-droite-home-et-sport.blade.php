1169c9c1829bbdc4ca7391fc92a8b6676144f5e9
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-05 08:19:09 +0100

    Couleur du fin actu

--- resources/views/onglet/section-droite-home-et-sport.blade.php
+++ resources/views/onglet/section-droite-home-et-sport.blade.php
@@ -24,8 +24,8 @@
                         {!! $actu->date_fil_actu !!}
                     @endif
                 </div>
-                <div class="col-10 p-2">
-                    <div class="categorie col-12 text-secondary p-0">{{ $actu->categorie }}</div>
+                <div class="font-size-1-rem col-10 p-2">
+                    <div class="col-12 text-primary p-0">{{ $actu->categorie }}</div>
                     <div class="col-12 p-0">{!! $actu->preambule !!}</div>
                 </div>
             </div>
