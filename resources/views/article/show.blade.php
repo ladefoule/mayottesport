1cc6617a15d86e6409e4a2fbf3af33fa58877938
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-14 11:44:20 +0100

    espacement entre les articles suivants/prec

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -49,13 +49,13 @@
                     <div class="col-6 d-flex justify-content-start flex-wrap mb-auto">
                         @if($articlePrecedent)
                             <span class="col-12 p-0">{!! config('listes.boutons.left') !!} Précédent</span>
-                            <a class="col-12 p-0 pr-3 mb-auto" href="{{ $articlePrecedent->href }}">{{ $articlePrecedent->titre }}</a>
+                            <a class="col-12 p-0 mb-auto" href="{{ $articlePrecedent->href }}">{{ $articlePrecedent->titre }}</a>
                         @endif
                     </div>
                     <div class="col-6 d-flex justify-content-end flex-wrap text-right mb-auto">
                         @if($articleSuivant)
                             <span class="col-12 p-0">Suivant {!! config('listes.boutons.right') !!}</span>
-                            <a class="col-12 p-0 pl-3 mb-auto" href="{{ $articleSuivant->href }}">{{ $articleSuivant->titre }}</a>
+                            <a class="col-12 p-0 mb-auto" href="{{ $articleSuivant->href }}">{{ $articleSuivant->titre }}</a>
                         @endif
                     </div>
                 </div>
