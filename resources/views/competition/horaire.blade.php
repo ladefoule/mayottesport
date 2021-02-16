74d08ec8045bfbe95fb960c1a29bdb3144d7f733
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-16 21:29:03 +0100

    Image webp sur la page match

--- resources/views/competition/horaire.blade.php
+++ resources/views/competition/horaire.blade.php
@@ -13,7 +13,7 @@
 <div class="p-lg-3 h-100">
     <form action="" method="post" id="formulaire">
         @csrf
-        <div class="row m-0 text-white bloc-match bloc-match-{{ $sport->slug }} py-4" style="background-image: url('{{ asset('storage/img/sport/'.$sport->slug.'.jpg') }}')">
+        <div class="row m-0 text-white bloc-match bloc-match-{{ $sport->slug }} py-4" style="background-image: url('{{ asset('storage/img/sport/'.$sport->slug.'.webp') }}')">
             <div class="row mx-0 col-5 d-flex justify-content-between align-items-center bloc-equipe-dom p-1 mb-5">
                 <div class="col-md-4 col-lg-12 col-xl-4 py-2 px-0">
                     <a href="{{ $match->href_equipe_dom }}"><img src="{{ $match->fanion_equipe_dom }}" alt="{{ $match->equipe_dom->nom }}" class="fanion-match"></a>
--- resources/views/competition/match.blade.php
+++ resources/views/competition/match.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="p-lg-3 h-100">
-    <div class="row m-0 text-white bloc-match bloc-match-{{ $sport->slug }} py-4" style="background-image: url('{{ asset('storage/img/sport/'.$sport->slug.'.jpg') }}')">
+    <div class="row m-0 text-white bloc-match bloc-match-{{ $sport->slug }} py-4" style="background-image: url('{{ asset('storage/img/sport/'.$sport->slug.'.webp') }}')">
         <div class="col-12 p-0 d-flex align-items-center mb-5">
             <div class="row m-auto w-40 d-flex justify-content-between align-items-center bloc-equipe-dom p-1 mb-5">
                 <div class="col-md-4 col-lg-12 col-xl-4 py-2 px-0">
