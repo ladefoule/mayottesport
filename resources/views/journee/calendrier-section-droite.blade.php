a007e02573daf8221cccc34344f4ccab005f3bc0
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-16 21:16:09 +0100

    Suppression des fanions dans le bloc de droite

--- resources/views/journee/calendrier-section-droite.blade.php
+++ resources/views/journee/calendrier-section-droite.blade.php
@@ -18,22 +18,17 @@
         ?>
         <a href="{{ $match->url }}" class="text-decoration-none text-body match-calendrier">
             <div class="row d-flex flex-nowrap py-2 border-bottom @if($i==0) border-top @endif">
-                <div class="bloc-dom p-0 d-flex justify-content-between align-items-center @if($equipeDomVainqueur) font-weight-bold @endif">
+                <div class="bloc-dom p-0 d-flex justify-content-start text-left align-items-center @if($equipeDomVainqueur) font-weight-bold @endif">
                     {{ $match->equipe_dom->nom }}
                 </div>
                 <div class="bloc-sc d-flex flex-wrap justify-content-center align-items-center p-0">
-                    <span class="col-12 p-0 text-center score">{!! $match->score !!} @if($avecProlongations) ap. @endif</span>
+                    <span class="col-12 p-0 text-center score">{!! $match->score !!}</span>
                     @if ($avecTab)
                         <span class="col-12 p-0 text-center tirs-au-but">tab. {{ $match->tab_eq_dom . '-' . $match->tab_eq_ext }}</span>
                     @endif
                 </div>
-                <div class="bloc-ext p-0 d-flex justify-content-between align-items-center @if($equipeExtVainqueur) font-weight-bold @endif">
-                    <div class="equipe-exterieur">
-                        {{ $match->equipe_ext->nom }}
-                    </div>
-                    <div class="fanion-calendrier  pl-xl-1">
-                        <img src="{{ $match->fanion_equipe_ext }}" alt="{{ $match->equipe_ext->nom }}">
-                    </div>
+                <div class="bloc-ext p-0 d-flex align-items-center justify-content-end text-right @if($equipeExtVainqueur) font-weight-bold @endif">
+                    {{ $match->equipe_ext->nom }}
                 </div>
             </div>
         </a>
