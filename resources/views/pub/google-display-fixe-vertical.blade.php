c07474a0996174332023fecb798bf335c4fd813f
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-17 12:05:00 +0100

    test pub vertical width auto

--- resources/views/pub/google-display-fixe-vertical.blade.php
+++ resources/views/pub/google-display-fixe-vertical.blade.php
@@ -1,6 +1,6 @@
 <!-- display fixe vertical -->
 <ins class="adsbygoogle mx-auto d-flex justify-content-center"
-     style="display:inline-block;width:300px;height:600px"
+     style="display:inline-block;width:auto;height:600px"
      data-ad-client="ca-pub-6802785230681286"
      data-ad-slot="9283727371"></ins>
 <script>
