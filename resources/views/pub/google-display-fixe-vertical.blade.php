8dbdb175620e9f72c11f0986b74870e8439b2a9e
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-17 12:07:57 +0100

    test 2 pub vertical

--- resources/views/pub/google-display-fixe-vertical.blade.php
+++ resources/views/pub/google-display-fixe-vertical.blade.php
@@ -1,6 +1,6 @@
 <!-- display fixe vertical -->
-<ins class="adsbygoogle mx-auto d-flex justify-content-center"
-     style="display:inline-block;width:auto;height:600px"
+<ins class="adsbygoogle mx-auto p-0 col-12 overflow-x-hidden overflow-y-hidden d-flex justify-content-center"
+     style="height:600px"
      data-ad-client="ca-pub-6802785230681286"
      data-ad-slot="9283727371"></ins>
 <script>
