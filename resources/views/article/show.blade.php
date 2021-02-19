6dba722580e3ebde2ea6abc34949b6b0856558cc
ladefoule <67441253+ladefoule@users.noreply.github.com>
2021-02-19 15:15:31 +0100

    Image sur toute la largeur dans la page article 2

--- resources/views/article/show.blade.php
+++ resources/views/article/show.blade.php
@@ -16,7 +16,7 @@
             <h1 class="col-12 titre-page-article py-3">
                 {{ $article->titre }}
             </h1>
-            <div class="col-12 d-flex m-auto justify-content-center">
+            <div class="row d-flex m-auto justify-content-center">
                 <img src="{{ $article->img }}" alt="{{ $article->titre }}" title="{{ $article->titre }}" class="img-fluid">
             </div>
             <div class="col-12 text-secondary">{{ $article->img_description }}</div>
